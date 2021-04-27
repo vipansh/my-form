b35dff0870ee1e55ddf54c42ba9c1a7a739e69b5
Vipansh Thakur <vipansh@github.com>
2021-04-27 13:03:16 +0530

    Qr code bug fixed

--- src/componants/SubmittedForm/SubmittedFormHeader.js
+++ src/componants/SubmittedForm/SubmittedFormHeader.js
@@ -1,108 +1,209 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-import QRCode from 'react-qr-code';
-import { useAuth } from '../../Context/AuthContext';
+import React from "react";
+import { Link } from "react-router-dom";
+import QRCode from "react-qr-code";
+import { useAuth } from "../../Context/AuthContext";
 
-export const SubmittedFormHeader = ({ id, value, title, }) => {
-    const { idOpen, setIdOpen } = value
+export const SubmittedFormHeader = ({ id, value, title }) => {
+  const { idOpen, setIdOpen } = value;
 
+  const [showSideBar, setShowSideBar] = React.useState(false);
+  return (
+    <div className="border-b shadow-lg ">
+      {showSideBar ? (
+        <SideBar value={{ showSideBar, setShowSideBar }} id={id} />
+      ) : null}
 
-    const [showSideBar, setShowSideBar] = React.useState(false)
-    return (
-        <div className="border-b shadow-lg " >
-            {showSideBar ? <SideBar value={{ showSideBar, setShowSideBar }} id={id} /> : null}
+      <header className=" md:flex md:items-center md:justify-between p-4 pb-0  md:pb-4">
+        <div className=" md:flex md:items-center">
+          <Link to="/" className="mx-4 my-auto text-gray-800">
+            <svg
+              className="text-gray-800 h-4 w-4 inline"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 20 20"
+              fill="currentColor"
+            >
+              <path d="M10.707 2.293a1 1 0 00-1.414 0l-7 7a1 1 0 001.414 1.414L4 10.414V17a1 1 0 001 1h2a1 1 0 001-1v-2a1 1 0 011-1h2a1 1 0 011 1v2a1 1 0 001 1h2a1 1 0 001-1v-6.586l.293.293a1 1 0 001.414-1.414l-7-7z" />
+            </svg>
+          </Link>
+          {title.length > 0 ? title : "Untitled Form"}
+        </div>
+        <nav
+          className="w-auto text-gray-700 flex justify-center flex-row items-center
+                "
+        >
+          <button
+            className="mx-4 my-4 inline-flex  text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg "
+            onClick={() => {
+              setShowSideBar(true);
+            }}
+          >
+            {/* <Link to={`/form/b/fill/${id}`} className="" >Share</Link></button> */}
+            Share
+          </button>
 
-            <header className=" md:flex md:items-center md:justify-between p-4 pb-0  md:pb-4">
-                <div className=" md:flex md:items-center">
-                    <Link to="/" className="mx-4 my-auto text-gray-800"><svg className="text-gray-800 h-4 w-4 inline" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
-                        <path d="M10.707 2.293a1 1 0 00-1.414 0l-7 7a1 1 0 001.414 1.414L4 10.414V17a1 1 0 001 1h2a1 1 0 001-1v-2a1 1 0 011-1h2a1 1 0 011 1v2a1 1 0 001 1h2a1 1 0 001-1v-6.586l.293.293a1 1 0 001.414-1.414l-7-7z" />
-                    </svg></Link>
-                    {title.length > 0 ? title : "Untitled Form"}
-                </div>
-                <nav className="w-auto text-gray-700 flex justify-center flex-row items-center
-                ">
+          <span>
+            <img
+              alt="Placeholder"
+              className="inline mx-4 rounded-full"
+              src="https://picsum.photos/32/32/?random"
+            />
+          </span>
+        </nav>
+      </header>
 
+      <div className="flex justify-center ">
+        <div
+          onClick={() => {
+            setIdOpen(1);
+          }}
+          className={
+            "mx-4 p-4 py-2  cursor-pointer" +
+            (idOpen === 1
+              ? " font-semibold border-blue-600 border-b-4 text-blue-600"
+              : "")
+          }
+        >
+          {" "}
+          Questions
+        </div>
 
-                    <button className="mx-4 my-4 inline-flex  text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg " onClick={() => { setShowSideBar(true) }}>
-                        {/* <Link to={`/form/b/fill/${id}`} className="" >Share</Link></button> */}
-
-Share</button>
-
-
-                    <span>
-                        <img alt="Placeholder" className="inline mx-4 rounded-full" src="https://picsum.photos/32/32/?random" />
-
-                    </span>
-
-
-
-
-                </nav>
-
-
-
-            </header >
-
-
-            <div className="flex justify-center ">
-                <div onClick={() => { setIdOpen(1) }} className={"mx-4 p-4 py-2  cursor-pointer" +
-                    (idOpen === 1 ? " font-semibold border-blue-600 border-b-4 text-blue-600" : "")} >  Questions</div>
-
-                <div onClick={() => { setIdOpen(2) }} className={"mx-4 p-4 py-2  cursor-pointer" +
-                    (idOpen === 2 ? " font-semibold border-blue-600 border-b-4 text-blue-600" : "")}>  Responses</div>
-            </div>
-        </div >
-
-
-
-
-
-
-    )
-}
-
-
-
-
+        <div
+          onClick={() => {
+            setIdOpen(2);
+          }}
+          className={
+            "mx-4 p-4 py-2  cursor-pointer" +
+            (idOpen === 2
+              ? " font-semibold border-blue-600 border-b-4 text-blue-600"
+              : "")
+          }
+        >
+          {" "}
+          Responses
+        </div>
+      </div>
+    </div>
+  );
+};
 
 const SideBar = ({ value, id }) => {
-    const { currentUser } = useAuth()
+  const { currentUser } = useAuth();
 
+  const domain = window.location.hostname;
 
+  const { showSideBar, setShowSideBar } = value;
+  const tourl = `https%3A%2F%2F${domain}%2Fform%2Fb%2Ffill%2F${id}%2F${currentUser.uid}`;
+  const text = `Fill%20this%20survey%20${tourl}`;
+  return (
+    <transition name="slide z-50 slide-enter-active transition-slow ease-in transition-transform slide-enter ">
+      <div v-if="showmenu">
+        <button
+          onClick={() => {
+            setShowSideBar(false);
+          }}
+          className="fixed top-0 right-0 bottom-0 left-0 h-full w-full bg-black opacity-50"
+        >
+          X
+        </button>
+        <div
+          onClick={() => {
+            setShowSideBar(false);
+          }}
+          className=" md:w-1/2  w-10/12 min-h-screen p-8 bg-gray-200 fixed right-0 top-0 shadow-2xl ease-in-out  transition-transform transition-medium"
+        >
+          <p className="md:mt-16 text-center  text-gray-600 flex flex-col justify-center items-center  text-xl">
+            <a
+              href={`/form/b/fill/${id}/${currentUser.uid}`}
+              target="_blank"
+              rel="noreferrer noopener"
+            >
+              link to form
+            </a>
 
-    const domain = window.location.hostname
+            <a
+              href={`https://api.whatsapp.com/send?text=${text}`}
+              target="_blank"
+              rel="noreferrer noopener"
+              aria-label="Share on WhatsApp"
+              title="Share on WhatsApp"
+              className="block py-3 px-2 my-2 bg-green-300 text-gray-800 rounded-lg font-bold  border border-green-600 w-64"
+            >
+              {" "}
+              <svg
+                className="w-4 h-4 inline mx-2"
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 24 24"
+              >
+                <path d="M20.1 3.9C17.9 1.7 15 .5 12 .5 5.8.5.7 5.6.7 11.9c0 2 .5 3.9 1.5 5.6L.6 23.4l6-1.6c1.6.9 3.5 1.3 5.4 1.3 6.3 0 11.4-5.1 11.4-11.4-.1-2.8-1.2-5.7-3.3-7.8zM12 21.4c-1.7 0-3.3-.5-4.8-1.3l-.4-.2-3.5 1 1-3.4L4 17c-1-1.5-1.4-3.2-1.4-5.1 0-5.2 4.2-9.4 9.4-9.4 2.5 0 4.9 1 6.7 2.8 1.8 1.8 2.8 4.2 2.8 6.7-.1 5.2-4.3 9.4-9.5 9.4zm5.1-7.1c-.3-.1-1.7-.9-1.9-1-.3-.1-.5-.1-.7.1-.2.3-.8 1-.9 1.1-.2.2-.3.2-.6.1s-1.2-.5-2.3-1.4c-.9-.8-1.4-1.7-1.6-2-.2-.3 0-.5.1-.6s.3-.3.4-.5c.2-.1.3-.3.4-.5.1-.2 0-.4 0-.5C10 9 9.3 7.6 9 7c-.1-.4-.4-.3-.5-.3h-.6s-.4.1-.7.3c-.3.3-1 1-1 2.4s1 2.8 1.1 3c.1.2 2 3.1 4.9 4.3.7.3 1.2.5 1.6.6.7.2 1.3.2 1.8.1.6-.1 1.7-.7 1.9-1.3.2-.7.2-1.2.2-1.3-.1-.3-.3-.4-.6-.5z"></path>
+              </svg>
+              Whatsapp
+            </a>
 
-    const { showSideBar, setShowSideBar } = value
-    const tourl = `https%3A%2F%2F${domain}%2Fform%2Fb%2Ffill%2F${id}%2F${currentUser.uid}`
-    const text = `Fill%20this%20survey%20${tourl}`
-    return (
-        <transition name="slide z-50 slide-enter-active transition-slow ease-in transition-transform slide-enter ">
-            <div v-if="showmenu">
-                <button onClick={() => { setShowSideBar(false) }} className="fixed top-0 right-0 bottom-0 left-0 h-full w-full bg-black opacity-50">X</button>
-                <div onClick={() => { setShowSideBar(false) }} className=" md:w-1/2  w-10/12 min-h-screen p-8 bg-gray-200 fixed right-0 top-0 shadow-2xl ease-in-out  transition-transform transition-medium">
+            <a
+              href={`https://twitter.com/intent/tweet/?text=${text}`}
+              target="_blank"
+              rel="noreferrer noopener"
+              aria-label="Share on Twitter"
+              title="Share on Twitter"
+              className="block py-3 px-2 my-2 bg-green-300 text-gray-800 rounded-lg font-bold  border border-green-600 w-64"
+            >
+              {" "}
+              <svg
+                className="w-4 h-4 inline mx-2"
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 24 24"
+              >
+                <path d="M23.44 4.83c-.8.37-1.5.38-2.22.02.93-.56.98-.96 1.32-2.02-.88.52-1.86.9-2.9 1.1-.82-.88-2-1.43-3.3-1.43-2.5 0-4.55 2.04-4.55 4.54 0 .36.03.7.1 1.04-3.77-.2-7.12-2-9.36-4.75-.4.67-.6 1.45-.6 2.3 0 1.56.8 2.95 2 3.77-.74-.03-1.44-.23-2.05-.57v.06c0 2.2 1.56 4.03 3.64 4.44-.67.2-1.37.2-2.06.08.58 1.8 2.26 3.12 4.25 3.16C5.78 18.1 3.37 18.74 1 18.46c2 1.3 4.4 2.04 6.97 2.04 8.35 0 12.92-6.92 12.92-12.93 0-.2 0-.4-.02-.6.9-.63 1.96-1.22 2.56-2.14z"></path>
+              </svg>
+              Twitter
+            </a>
 
+            <a
+              href={`https://www.linkedin.com/sharing/share-offsite/?url=${tourl}`}
+              target="_blank"
+              rel="noreferrer noopener"
+              aria-label="Share on LinkdIn"
+              title="Share on LinkdIn"
+              className="block py-3 px-2 my-2 bg-green-300 text-gray-800 rounded-lg font-bold  border border-green-600 w-64"
+            >
+              <svg
+                className="w-4 h-4 inline mx-2"
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 24 24"
+              >
+                <path d="M6.5 21.5h-5v-13h5v13zM4 6.5C2.5 6.5 1.5 5.3 1.5 4s1-2.4 2.5-2.4c1.6 0 2.5 1 2.6 2.5 0 1.4-1 2.5-2.6 2.5zm11.5 6c-1 0-2 1-2 2v7h-5v-13h5V10s1.6-1.5 4-1.5c3 0 5 2.2 5 6.3v6.7h-5v-7c0-1-1-2-2-2z"></path>
+              </svg>
+              LinkdIn
+            </a>
 
-                    <p className="md:mt-16 text-center  text-gray-600 flex flex-col justify-center items-center  text-xl" >
+            <a
+              href={`https://telegram.me/share/url?text=&url=${tourl}`}
+              target="_blank"
+              rel="noreferrer noopener"
+              aria-label="Share on Telegram"
+              title="Share on Telegram"
+              className="block py-3 px-2 my-2 bg-green-300 text-gray-800 rounded-lg font-bold  border border-green-600 w-64"
+            >
+              <svg
+                className="w-4 h-4 inline mx-2"
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 24 24"
+              >
+                <path d="M.707 8.475C.275 8.64 0 9.508 0 9.508s.284.867.718 1.03l5.09 1.897 1.986 6.38a1.102 1.102 0 0 0 1.75.527l2.96-2.41a.405.405 0 0 1 .494-.013l5.34 3.87a1.1 1.1 0 0 0 1.046.135 1.1 1.1 0 0 0 .682-.803l3.91-18.795A1.102 1.102 0 0 0 22.5.075L.706 8.475z"></path>
+              </svg>{" "}
+              Telegram
+            </a>
 
-                        <a href={`/form/b/fill/${id}/${currentUser.uid}`} target="_blank" rel="noreferrer noopener">link to form</a>
-
-
-                        <a href={`https://api.whatsapp.com/send?text=${text}`} target="_blank" rel="noreferrer noopener" aria-label="Share on WhatsApp" title="Share on WhatsApp" className="block py-3 px-2 my-2 bg-green-300 text-gray-800 rounded-lg font-bold  border border-green-600 w-64"> <svg className="w-4 h-4 inline mx-2" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M20.1 3.9C17.9 1.7 15 .5 12 .5 5.8.5.7 5.6.7 11.9c0 2 .5 3.9 1.5 5.6L.6 23.4l6-1.6c1.6.9 3.5 1.3 5.4 1.3 6.3 0 11.4-5.1 11.4-11.4-.1-2.8-1.2-5.7-3.3-7.8zM12 21.4c-1.7 0-3.3-.5-4.8-1.3l-.4-.2-3.5 1 1-3.4L4 17c-1-1.5-1.4-3.2-1.4-5.1 0-5.2 4.2-9.4 9.4-9.4 2.5 0 4.9 1 6.7 2.8 1.8 1.8 2.8 4.2 2.8 6.7-.1 5.2-4.3 9.4-9.5 9.4zm5.1-7.1c-.3-.1-1.7-.9-1.9-1-.3-.1-.5-.1-.7.1-.2.3-.8 1-.9 1.1-.2.2-.3.2-.6.1s-1.2-.5-2.3-1.4c-.9-.8-1.4-1.7-1.6-2-.2-.3 0-.5.1-.6s.3-.3.4-.5c.2-.1.3-.3.4-.5.1-.2 0-.4 0-.5C10 9 9.3 7.6 9 7c-.1-.4-.4-.3-.5-.3h-.6s-.4.1-.7.3c-.3.3-1 1-1 2.4s1 2.8 1.1 3c.1.2 2 3.1 4.9 4.3.7.3 1.2.5 1.6.6.7.2 1.3.2 1.8.1.6-.1 1.7-.7 1.9-1.3.2-.7.2-1.2.2-1.3-.1-.3-.3-.4-.6-.5z"></path></svg>Whatsapp</a>
-
-                        <a href={`https://twitter.com/intent/tweet/?text=${text}`} target="_blank" rel="noreferrer noopener" aria-label="Share on Twitter" title="Share on Twitter" className="block py-3 px-2 my-2 bg-green-300 text-gray-800 rounded-lg font-bold  border border-green-600 w-64"> <svg className="w-4 h-4 inline mx-2" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M23.44 4.83c-.8.37-1.5.38-2.22.02.93-.56.98-.96 1.32-2.02-.88.52-1.86.9-2.9 1.1-.82-.88-2-1.43-3.3-1.43-2.5 0-4.55 2.04-4.55 4.54 0 .36.03.7.1 1.04-3.77-.2-7.12-2-9.36-4.75-.4.67-.6 1.45-.6 2.3 0 1.56.8 2.95 2 3.77-.74-.03-1.44-.23-2.05-.57v.06c0 2.2 1.56 4.03 3.64 4.44-.67.2-1.37.2-2.06.08.58 1.8 2.26 3.12 4.25 3.16C5.78 18.1 3.37 18.74 1 18.46c2 1.3 4.4 2.04 6.97 2.04 8.35 0 12.92-6.92 12.92-12.93 0-.2 0-.4-.02-.6.9-.63 1.96-1.22 2.56-2.14z"></path></svg>Twitter</a>
-
-                        <a href={`https://www.linkedin.com/sharing/share-offsite/?url=${tourl}`} target="_blank" rel="noreferrer noopener" aria-label="Share on LinkdIn" title="Share on LinkdIn" className="block py-3 px-2 my-2 bg-green-300 text-gray-800 rounded-lg font-bold  border border-green-600 w-64"><svg className="w-4 h-4 inline mx-2" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M6.5 21.5h-5v-13h5v13zM4 6.5C2.5 6.5 1.5 5.3 1.5 4s1-2.4 2.5-2.4c1.6 0 2.5 1 2.6 2.5 0 1.4-1 2.5-2.6 2.5zm11.5 6c-1 0-2 1-2 2v7h-5v-13h5V10s1.6-1.5 4-1.5c3 0 5 2.2 5 6.3v6.7h-5v-7c0-1-1-2-2-2z"></path></svg>LinkdIn</a>
-
-                        <a href={`https://telegram.me/share/url?text=&url=${tourl}`} target="_blank" rel="noreferrer noopener" aria-label="Share on Telegram" title="Share on Telegram" className="block py-3 px-2 my-2 bg-green-300 text-gray-800 rounded-lg font-bold  border border-green-600 w-64"><svg className="w-4 h-4 inline mx-2" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M.707 8.475C.275 8.64 0 9.508 0 9.508s.284.867.718 1.03l5.09 1.897 1.986 6.38a1.102 1.102 0 0 0 1.75.527l2.96-2.41a.405.405 0 0 1 .494-.013l5.34 3.87a1.1 1.1 0 0 0 1.046.135 1.1 1.1 0 0 0 .682-.803l3.91-18.795A1.102 1.102 0 0 0 22.5.075L.706 8.475z"></path></svg> Telegram</a>
-
-                        <div className=" my-8"> <QRCode value={`https://${domain}/form/b/fill/${id}`} />,</div>
-
-
-
-
-                    </p>
-                </div >
-            </div >
-        </transition >
-    )
-}
+            <div className=" my-8">
+              {" "}
+              <QRCode
+                value={`https://${domain}/form/b/fill/${id}/${currentUser.uid}`}
+              />
+              ,
+            </div>
+          </p>
+        </div>
+      </div>
+    </transition>
+  );
+};
--- src/pages/LogIn.js
+++ src/pages/LogIn.js
@@ -1,120 +1,124 @@
-
-
-
-import React, { useCallback, useContext, useState } from "react"
-import { Redirect, useHistory } from "react-router-dom"
+import React, { useCallback, useContext, useState } from "react";
+import { Redirect, useHistory } from "react-router-dom";
 
 import { useAuth } from "../Context/AuthContext";
 export const LogIn = ({ history }) => {
+  const { currentUser, login, signup } = useAuth();
 
-    const { currentUser, login, signup } = useAuth()
+  const [isErr, setIsErr] = useState(false);
+  const [errMsg, setErrMsg] = useState("");
+  const handleLogin = useCallback(
+    async (event) => {
+      event.preventDefault();
+      const { email, password } = event.target.elements;
+      try {
+        await signup(email.value, password.value);
+        history.push("/");
+      } catch (err) {
+        try {
+          await login(email.value, password.value);
+          history.push("/");
+        } catch (error) {
+          setIsErr(true);
+          console.log(error);
+          setErrMsg(error.message);
+          setTimeout(() => {
+            setIsErr(false);
+          }, 3000);
+        }
+      }
+    },
+    [history]
+  );
 
-    const [isErr, setIsErr] = useState(false)
-    const [errMsg, setErrMsg] = useState("")
-    const handleLogin = useCallback(
-        async event => {
-            event.preventDefault();
-            const { email, password } = event.target.elements;
-            try {
-                await signup(email.value, password.value);
-                history.push("/");
-            } catch (err) {
-                try {
-                    await login(email.value, password.value);
-                    history.push("/");
-                } catch (error) {
-                    setIsErr(true)
-                    console.log(error);
-                    setErrMsg(error.message)
+  if (currentUser) {
+    return <Redirect to="/" />;
+  }
 
-                    setTimeout(() => { setIsErr(false) }, 3000)
-                }
-            }
-        },
-
-        [history]
-
-
-    );
-
-
-
-
-    if (currentUser) {
-        return <Redirect to="/" />;
-    }
-
-
-
-    return (
-
-        <div className="min-h-screen bg-gray-100 text-gray-900 flex justify-center">
-            <div
-                className="max-w-screen-xl m-0 sm:m-20 bg-white shadow sm:rounded-lg flex justify-center flex-1"
+  return (
+    <div className="min-h-screen bg-gray-100 text-gray-900 flex justify-center">
+      <div className="max-w-screen-xl m-0 sm:m-20 bg-white shadow sm:rounded-lg flex justify-center flex-1">
+        <div className="lg:w-1/2 xl:w-5/12 p-6 sm:p-12">
+          <div>
+            <svg
+              className="w-32 mx-auto text-indigo-500 "
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
             >
-                <div className="lg:w-1/2 xl:w-5/12 p-6 sm:p-12">
-                    <div>
-                        <svg className="w-32 mx-auto text-indigo-500 " xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 6.253v13m0-13C10.832 5.477 9.246 5 7.5 5S4.168 5.477 3 6.253v13C4.168 18.477 5.754 18 7.5 18s3.332.477 4.5 1.253m0-13C13.168 5.477 14.754 5 16.5 5c1.747 0 3.332.477 4.5 1.253v13C19.832 18.477 18.247 18 16.5 18c-1.746 0-3.332.477-4.5 1.253" />
-                        </svg>
-                    </div>
-                    <div className="mt-12 flex flex-col items-center">
-                        <h1 className="text-2xl xl:text-3xl font-extrabold">
-                            Sign up for Me Form
-                          </h1>
-                        {isErr && <div className="p-2 w-full text-white rounded-lg bg-red-600 ">{errMsg}</div>}
-                        <div className="w-full flex-1 mt-8">
-
-
-                            <form className="mx-auto max-w-xs" onSubmit={(e) => { handleLogin(e) }}>
-                                <input
-                                    className="w-full px-8 py-4 rounded-lg font-medium bg-gray-100 border border-gray-200 placeholder-gray-500 text-sm focus:outline-none focus:border-gray-400 focus:bg-white"
-                                    type="text"
-                                    placeholder="Email"
-                                    required
-                                    name="email"
-                                />
-                                <input
-                                    className="w-full px-8 py-4 rounded-lg font-medium bg-gray-100 border border-gray-200 placeholder-gray-500 text-sm focus:outline-none focus:border-gray-400 focus:bg-white mt-5"
-                                    type="password"
-                                    placeholder="Password"
-                                    required
-                                    name="password"
-                                />
-                                <div className="flex justify-around">
-                                    <button
-                                        className="m-2 tracking-wide font-semibold bg-indigo-500 text-gray-100 w-full py-4 rounded-lg hover:bg-indigo-700 transition-all duration-300 ease-in-out flex items-center justify-center focus:shadow-outline focus:outline-none"
-                                        type="submit">
-                                        <svg
-                                            className="w-6 h-6 -ml-2"
-                                            fill="none"
-                                            stroke="currentColor"
-                                            strokeWidth="2"
-                                            strokeLinecap="round"
-                                            strokeLinejoin="round"
-                                        >
-                                            <path d="M16 21v-2a4 4 0 00-4-4H5a4 4 0 00-4 4v2" />
-                                            <circle cx="8.5" cy="7" r="4" />
-                                            <path d="M20 8v6M23 11h-6" />
-                                        </svg>
-                                        <span className="ml-3">
-                                            Sign Up/Log In
-                </span>
-                                    </button>
-
-                                </div>
-                            </form>
-                        </div>
-                    </div>
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                d="M12 6.253v13m0-13C10.832 5.477 9.246 5 7.5 5S4.168 5.477 3 6.253v13C4.168 18.477 5.754 18 7.5 18s3.332.477 4.5 1.253m0-13C13.168 5.477 14.754 5 16.5 5c1.747 0 3.332.477 4.5 1.253v13C19.832 18.477 18.247 18 16.5 18c-1.746 0-3.332.477-4.5 1.253"
+              />
+            </svg>
+          </div>
+          <div className="mt-12 flex flex-col items-center">
+            <h1 className="text-2xl xl:text-3xl font-extrabold">
+              Sign up for Me Form
+            </h1>
+            {isErr && (
+              <div className="p-2 w-full text-white rounded-lg bg-red-600 ">
+                {errMsg}
+              </div>
+            )}
+            <div className="w-full flex-1 mt-8">
+              <form
+                className="mx-auto max-w-xs"
+                onSubmit={(e) => {
+                  handleLogin(e);
+                }}
+              >
+                <input
+                  className="w-full px-8 py-4 rounded-lg font-medium bg-gray-100 border border-gray-200 placeholder-gray-500 text-sm focus:outline-none focus:border-gray-400 focus:bg-white"
+                  type="text"
+                  placeholder="Email"
+                  required
+                  name="email"
+                />
+                <input
+                  className="w-full px-8 py-4 rounded-lg font-medium bg-gray-100 border border-gray-200 placeholder-gray-500 text-sm focus:outline-none focus:border-gray-400 focus:bg-white mt-5"
+                  type="password"
+                  placeholder="Password"
+                  required
+                  name="password"
+                />
+                <div className="flex justify-around">
+                  <button
+                    className="m-2 tracking-wide font-semibold bg-indigo-500 text-gray-100 w-full py-4 rounded-lg hover:bg-indigo-700 transition-all duration-300 ease-in-out flex items-center justify-center focus:shadow-outline focus:outline-none"
+                    type="submit"
+                  >
+                    <svg
+                      className="w-6 h-6 -ml-2"
+                      fill="none"
+                      stroke="currentColor"
+                      strokeWidth="2"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                    >
+                      <path d="M16 21v-2a4 4 0 00-4-4H5a4 4 0 00-4 4v2" />
+                      <circle cx="8.5" cy="7" r="4" />
+                      <path d="M20 8v6M23 11h-6" />
+                    </svg>
+                    <span className="ml-3">Sign Up/Log In</span>
+                  </button>
                 </div>
-                <div className="flex-1 bg-indigo-100 text-center hidden lg:flex">
-                    <div
-                        className="m-12 xl:m-16 w-full bg-contain bg-center bg-no-repeat"
-                        style={{ backgroundImage: "url('https://storage.googleapis.com/devitary-image-host.appspot.com/15848031292911696601-undraw_designer_life_w96d.svg')" }}
-                    ></div>
-                </div>
+              </form>
             </div>
-
-        </div >
-    )
-}
+          </div>
+        </div>
+        <div className="flex-1 bg-indigo-100 text-center hidden lg:flex">
+          <div
+            className="m-12 xl:m-16 w-full bg-contain bg-center bg-no-repeat"
+            style={{
+              backgroundImage:
+                "url('https://storage.googleapis.com/devitary-image-host.appspot.com/15848031292911696601-undraw_designer_life_w96d.svg')",
+            }}
+          ></div>
+        </div>
+      </div>
+    </div>
+  );
+};
